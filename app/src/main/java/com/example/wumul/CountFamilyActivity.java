11c397827163092b09cb9e6270c65e1c8028b84d
GyuHyeon Jung <wjdrbgus8167@naver.com>
2023-04-05 10:42:55 +0900

    현재 CountFamilyActivity 부분에 다이얼로그 추가 및 오류 수정 중

--- app/src/main/java/com/example/wumul/CountFamilyActivity.java
+++ app/src/main/java/com/example/wumul/CountFamilyActivity.java
@@ -2,14 +2,21 @@ package com.example.wumul;
 
 import static android.content.ContentValues.TAG;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
+
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,11 +27,14 @@ public class CountFamilyActivity extends AppCompatActivity {
     private TextView family_count_text;
     private ImageButton plus_button, minus_button;
 
+    LinearLayout ll;
     int familyCount = 0;
+
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.count_family);
+
 
         family_count_text = findViewById(R.id.count_text);
         family_count_text.setText(familyCount+"");
@@ -61,11 +71,39 @@ public class CountFamilyActivity extends AppCompatActivity {
         Intent intent = new Intent();
         intent.putExtra("familyCount", familyCount);
 
+        ll = findViewById(R.id.ll);
         // setResult 메서드를 호출하여 결과값을 저장
         setResult(Activity.RESULT_OK, intent);
 
+        public void showAlertDiglog() {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("구성원 추가");
+            builder.setMessage("구성원의 이름을 입력해주세요");
+            for(int i = 0;i<familyCount;i++)
+            {
+                EditText et = new EditText(getApplicationContext());
+                LinearLayout.LayoutParams p = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                et.setLayoutParams(p);
+                et.setText("editText" + i + "번");
+                et.setId(familyCount);
+                ll.addView(et);
+
+            }
+
+            builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class );
+                    startActivity(intent);
+                }
+            });
+            builder.setCancelable(false);
+            AlertDialog dialog =builder.create();
+            dialog.show();
+        };
         // CountFamilyActivity 종료
-        finish();
+        //finish();
     }
 
 }
--- app/src/main/java/com/example/wumul/MainActivity.java
+++ app/src/main/java/com/example/wumul/MainActivity.java
@@ -4,29 +4,61 @@ import static android.content.ContentValues.TAG;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.widget.Toolbar;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+//import android.widget.Toolbar;
+
 
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private static final String TAG = "Main_Activity";
+    private int familyCount;
+    private ImageView ivMenu;
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
     private static final int REQUEST_CODE_FAMILY_COUNT = 1001;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ActionBar actionBar = getSupportActionBar();
+//        ActionBar actionBar = getSupportActionBar();
 
+
+
+        ivMenu=findViewById(R.id.iv_menu);
+        drawerLayout=findViewById(R.id.drawer);
+        toolbar=findViewById(R.id.toolbar);
+
+        //액션바 변경하기(들어갈 수 있는 타입 : Toolbar type
+        setSupportActionBar(toolbar);
+
+        ivMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: 클릭됨");
+                drawerLayout.openDrawer(Gravity.LEFT);
+            }
+        });
 
     }
 
@@ -35,8 +67,11 @@ public class MainActivity extends AppCompatActivity {
 
         // CountFamilyActivity에서 넘어온 결과 처리
         if (requestCode == REQUEST_CODE_FAMILY_COUNT && resultCode == Activity.RESULT_OK) {
-            int familyCount = data.getIntExtra("familyCount", 0);
+            familyCount = data.getIntExtra("familyCount", 0);
             // familyCount 값을 사용하여 원하는 작업을 수행
         }
+
+
     }
+
 }
